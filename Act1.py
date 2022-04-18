ac2c9e1fec901caabafe432ede4c19d52c9dea58
edualdono <72567670+edualdono@users.noreply.github.com>
2022-04-18 12:53:53 -0500

    Comentarios_agregados
    Se han agregado los comentarios y corregido el problema de posicion

--- Act1.py
+++ Act1.py
@@ -10,15 +10,18 @@ class Game:
     #event_vector = [0,0,0]
 
     def __init__(self):
+        #se inicializa el juego 
         pygame.init()
-
+        #se especifica el tamanio de la pantalla
         self.__window = pygame.display.set_mode((640, 480), 0 ,32)
+        #creamos un vector de booleanos para detectar cambios en el teclado
         self.event_vector= [False,False,False]
         self.__running = False
         self.__hero = Hero(self.__window.get_size())
         #se
 
     def run(self):
+        #en esta funcion corre el juego 
         self.__running = True
 
         last_time = pygame.time.get_ticks()
@@ -36,32 +39,42 @@ class Game:
         self.__quit()
 
     def __process_events(self):
+        #detectamos los posibles eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                #cerramos aplicacion si se cierra el programa
                 self.__running = False
             if event.type == pygame.KEYDOWN:
+                #esta en epera de presionar una tecla
                 if event.key == pygame.K_RIGHT:
+                    #se presiona flecha derecha
                     self.event_vector [0] = True
                     self.event_vector [1] = True 
                     #print(self.event_vector)
                 elif event.key == pygame.K_LEFT:
+                    #se presiona flecha izquierda
                     self.event_vector [0] = True
                     self.event_vector [2] = True 
                     #print(self.event_vector)
             if event.type == pygame.KEYUP:
+                #si se presiona una tecla se revisa si esta dejo de ser presionada
                 if event.key == pygame.K_RIGHT:
+                    #se dejo de presionar flexha derecha
                     self.event_vector [0] = False
                     self.event_vector [1] = False 
                     #print(self.event_vector)
                 elif event.key == pygame.K_LEFT:
+                    #se dej ode presionar flecha izquierda
                     self.event_vector [0] = False
                     self.event_vector [2] = False 
                     #print(self.event_vector)
 
     def __update(self, delta_time):
+        #se actualiza el personaje , envian los posibles eventos 
         self.__hero.update(delta_time,self.event_vector)
 
     def __render(self):
+        #se dibujan los elementos en la pantalla
         self.__window.fill((0,0,0))
         self.__hero.render(self.__window)
         pygame.display.update()
@@ -70,6 +83,7 @@ class Game:
         pygame.quit()
 
     def __calc_delta_time(self, last):
+        #se calcula el timepo en el cual va ser actualizado el sistema
         current = pygame.time.get_ticks()
         delta = current - last
         return delta, current
@@ -77,6 +91,7 @@ class Game:
 class Images:
 
     def __init__(self, location, pasos , secuencias, vel):
+        #clase de las imagenes que se usaran del personaje
         self.__cuadro = pygame.image.load(path.join(*location)).convert_alpha()
         self.__numpasos = pasos
         self.tamaniocuadro = pygame.math.Vector2(self.__cuadro.get_width() / pasos, self.__cuadro.get_height() / secuencias)
@@ -84,7 +99,7 @@ class Images:
         self.__inicio = 0
 
         self.__animacion = []
-
+        #se crean los cuadros que se utilizaran para las animaciones
         for n in range(secuencias):
             self.__animacion.append([])
             for j in range(pasos):
@@ -97,6 +112,7 @@ class Images:
         
 
     def render(self, dest, secuencia, position, stop):
+        #en caso de que no se reciba ninguna senal se debe quedar parado el personaje
         if stop:
             dest.blit(self.__cuadro, position, self.__animacion[0][0])
         else:
@@ -110,6 +126,7 @@ class Hero:
     velocidad = 1 / 5
 
     def __init__(self, window_size):
+        #se inicializa el personaje
         self.__heromove = Images(Hero.location, Hero.pasos, Hero.num_secuencias, Hero.velocidad)
         self.__derecha = True
         #print(self.__heromove.tamaniocuadro.y)
@@ -128,6 +145,7 @@ class Hero:
                 #caso dodne se presiona derecha
                 self.__derecha = True
                 if self.__position.x >= self.__window_size[0] - self.__heromove.tamaniocuadro.x:
+                    #evitamos quese slaga de la pantalla el eprsonaje
                     self.__position.x = self.__position.x
                 else:
                     self.__position.x += self.__speed * delta_time
@@ -138,11 +156,12 @@ class Hero:
                 #caso dodne se presiona la izquierda
                 self.__derecha = False
                 if self.__position.x <= 0:
+                    #evitamso que se salga de la pantalla el personaje
                     self.__position.x = 0
                 else:
                     self.__position.x -= self.__speed * delta_time
                 #print('presionado izquierda')
-                #print(self.__position)
+                #print(self.__position)      
             self.time = 5000
         else:
             #caso automatico
@@ -163,6 +182,7 @@ class Hero:
         self.__heromove.update(delta_time)
 
     def render(self, dest):
+        #dibujamso el personaje
         self.__heromove.render(dest, 0 if self.__derecha else 1, self.__position, self.stop)
 
 def main(args=None):
--- Act1_2.py
+++ Act1_2.py
@@ -1,6 +1,4 @@
-from operator import index
-from tkinter import S
-from turtle import pos
+
 import pygame
 import sys
 from os import path
@@ -14,15 +12,17 @@ class Game:
     def __init__(self):
         pygame.init()
 
+        self.mensaje = 'galgo corredor'
         self.__window = pygame.display.set_mode((640, 480), 0, 32)
         self.__running = False
         self.contador_fuente = 0
         self.event_vector= [False,False,False]
-        self.mensaje = ' HOLA JESUS!!! COMO VAS? '
+        self.mensaje = ' '+self.mensaje+' '
         self.time = 0
         self.__letrasesc = Letras(self.__window.get_size(), self.mensaje, self.contador_fuente)
     
     def run(self):
+        #en este modulo el sistema esta corriendo 
         self.__running = True
         last_time = pygame.time.get_ticks()
         time_since_last_update = 0
@@ -41,38 +41,49 @@ class Game:
 
     def __process_events(self):
 
+        #detectamos los posibles eventos
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
+                #cerramos aplicacion si se cierra el programa
                 self.__running = False
             if event.type == pygame.KEYDOWN:
+                 #esta en epera de presionar una tecla
                 if event.key == pygame.K_RIGHT:
+                    #se presiona flecha derecha
                     self.event_vector [0] = True
                     self.event_vector [1] = False
                     #print(self.event_vector)
                 elif event.key == pygame.K_LEFT:
+                    #se presiona flecha izquierda
                     self.event_vector [0] = False
                     self.event_vector [1] = True
                 elif event.key == pygame.K_SPACE:
+                    #se presiona espacio
                     self.event_vector [2] = True
             if event.type == pygame.KEYUP:
                 if event.key == pygame.K_SPACE:
+                    #se suelta el espacio
                     self.event_vector [2] = False 
 
     def __update(self, delta_time):
 
         if self.event_vector[2] and self.time < 0:
+            #caso en dodne se presiono el espacio
             self.time = 500
             if self.contador_fuente < 2 :
+                #cambiamos la fuente
                 self.contador_fuente += 1
             else:
                 self.contador_fuente = 0
             self.__letrasesc = Letras(self.__window.get_size(), self.mensaje,self.contador_fuente)
         else:
+            #se da tiempo para que se pueda cambiar de forma comoda la fuente
             self.time -= delta_time 
 
         self.__letrasesc.update(delta_time,self.event_vector)
 
     def __render(self):
+        #se dibujan los elementos en la pantalla
         self.__window.fill((0, 0, 0))
         self.__letrasesc.render(self.__window)
         pygame.display.update()
@@ -93,7 +104,7 @@ class LetterType:
         self.__cuadro.set_colorkey((0,0,0))
         self.tamanio = tamanio
         self.__letras = []
-
+        #inicializamso el mensaje a escribir
         for n in range(total):
             left = (n % letrasxlinea ) * self.tamanio[0]
             top = int(n / letrasxlinea) * self.tamanio[1]
@@ -102,6 +113,7 @@ class LetterType:
             self.__letras.append(pygame.Rect(left,top,width,height))
 
     def render(self, dest, letras, position):
+        #se dibujan las letras
         index = self.mapeo(letras)
         #print(position)
         dest.blit(self.__cuadro, position, self.__letras[index])
@@ -115,17 +127,17 @@ class LetterType:
             index = ascii - 64
         elif ascii >= 48 and ascii <= 57: # 0 - 9 
             index = ascii - 32
-        elif ascii >= 44 and ascii <= 47:
+        elif ascii >= 44 and ascii <= 47: # , - . /
             index = ascii - 32
-        elif ascii == 145 or ascii == 146:
+        elif ascii == 145 or ascii == 146: # ' '
             index = 7
-        elif ascii == 147 or ascii == 148:
+        elif ascii == 147 or ascii == 148: # " "
             index = 2
-        elif ascii >= 40 and ascii <=41: 
+        elif ascii >= 40 and ascii <=41: # ( )
             index = ascii - 32
-        elif ascii == 33:
+        elif ascii == 33: # !
             index = 1
-        elif ascii == 63:
+        elif ascii == 63: # ?
             index = 31
         else:
             index = 0
@@ -133,22 +145,25 @@ class LetterType:
         return index
 
 class Letras:
+    #seleccionamos las fuentes disponibles
     locations = []
     locations.append(["./BitmapFonts-main/bennyfnt.png"])
     locations.append(["./BitmapFonts-main/auto_f02r.png"])
     locations.append( ["./BitmapFonts-main/crikey_f.png"])
 
+    #damso las acaracteristicas de la fuente
     total = 59
     letrasxlinea = 10
     tamanio = (32,32)
 
     def __init__(self, window_size, text, num_fuente):
         #print(num_fuente)
+        #se inicializa la fuente
         self.__letras = LetterType(Letras.locations[num_fuente], Letras.total, Letras.letrasxlinea, Letras.tamanio)
         self.texto = list(text)
         self.index_act = 0
         self.velocidad = 1/10
-        self.position = pygame.math.Vector2(-window_size[0]*10,window_size[1]/2)
+        self.position = pygame.math.Vector2(-window_size[0] * len(text),window_size[1]/2)
         self.window_size = window_size
 
     def update(self, delta_time, event_vector):
@@ -158,16 +173,18 @@ class Letras:
             #print('cambio der')
             
         elif(event_vector[1]):
-            #caso a la izquierda
+            #caso donde va a la izquierda
             self.position.x -= (self.velocidad * delta_time)
             #print('cambio izq')
 
         else:
+            #caso inicial
             self.position.x -= (self.velocidad * delta_time)
 
         
     def render(self, destino):
-        letras_esc = int((self.window_size[0]*3 - self.position.x) / self.__letras.tamanio[0]) +1
+        #se dibujan las letras y se genera el movimiento
+        letras_esc = int((self.window_size[0] - self.position.x) / self.__letras.tamanio[0]) +1
         #print(letras_esc)
         for n in range (letras_esc):
             index = (self.index_act + n) % len(self.texto)
